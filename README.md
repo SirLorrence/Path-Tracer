f0b8221af70bf37372e1ea885c1de27423e92fee
Laurence <sirlaurence@protonmail.com>
2024-01-21 13:57:14 -0800

    Showcase items

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 A Ray Tracer Renderer using C++. This project is aimed at helping me understand the fundamentals of Ray-tracing and how it works.
 
-### Site Links: 
-- [Quick thoughts after finishing RayTracingInOneWeekend](https://www.laurencesadler.com/ray-tracer-part-1)
+### Links: 
+- [Site page, with a small write-up with issues I've faced with multi-threading](https://www.laurencesadler.com/ray-tracer)
 
 ## Features
 
@@ -16,31 +16,47 @@ A Ray Tracer Renderer using C++. This project is aimed at helping me understand 
   * Dielectrics
   * Positionable Camera
   * Defocus Blur (“depth of field”)
-- PNG image output  
+- PNG image output
+- Multi-Threading (with C++ STD and OpenMP)
+- Render Time Information (ms) 
+
 
 
 
 ## Installation + Build
 
-This program has been tested on Pop OS and MacOS but should work under any system that has cmake and a C++ compiler installed.
+This program has been tested on Pop OS and MacOS but should work under any system that has cmake and a C++ compiler installed. (Clion, Visual Studio)
 
-Once project files are downloaded, and you are currently in the project directory, you can generate the Makefile using the following command:
+Once project files are downloaded and you are currently in the project directory, you can generate the Makefile using the following command:
 
 ```
 mkdir build
 cd build/
 cmake ..
 ```
+Additionally in the CMakeLists.txt you can enabled different types of rendering, by default its single threaded.
+```
+Remove '#' to enable feature
+#add_compile_definitions(MTHREAD)
+#add_compile_definitions(MTHREAD_V2) 
+#add_compile_definitions(ENABLE_MP) <-- OpenMP Render
+#add_compile_definitions(MTHREAD_TILE) <--- Tile MT Render
+```
 Now you can build the binary by running:
 
 ```
 make
 ```
+
 To generate the image, run the output binary in the terminal/cmd, which will result in the file appearing in the "images" folder
+
+
+## Project status
+
+### On Hold
 
 ## Roadmap
 - Project Refactoring
-- CPU/GPU multi-threading to speed up rending
 - SDL Project conversion
 - Runtime Rendering (being able to move the camera in a graphics window)
 
@@ -49,15 +65,12 @@ To generate the image, run the output binary in the terminal/cmd, which will res
 ## Authors
 - Laurence Sadler - [SirLorrence](https://gitlab.com/SirLorrence)
 
-## License
-This code is released under the [MIT License](https://gitlab.com/SirLorrence/raytracer/-/blob/main/LICENSE).
-
-## Project status
-
-### In Development
-
 ## Built with
 - [Tiny PNG Output](https://www.nayuki.io/page/tiny-png-output) - to write PNG file
+- [OpenMP](https://www.openmp.org/)
+
+## License
+This code is released under the [MIT License](https://gitlab.com/SirLorrence/raytracer/-/blob/main/LICENSE).
 
 ## References
 - [_Ray Tracing in One Weekend_](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
@@ -66,4 +79,5 @@ This code is released under the [MIT License](https://gitlab.com/SirLorrence/ray
 - [3D Math Primer for Graphics and Game Development.](https://gamemath.com/) - Digitial and Physical 
 - [ChatGPT](chat.openai.com) - How to create a CMake file and cryptic compiler errors
 - [Adobe | What is a PPM file?](https://www.adobe.com/creativecloud/file-types/image/raster/ppm-file.html)
+
 
--- src/main.cc
+++ src/main.cc
@@ -10,30 +10,31 @@ int main() {
   // World Objects
   HittableList world;
   std::shared_ptr<Material> materialGround = std::make_shared<Lambertian>(Color(0.5, 0.5, 0.5));
-  world.Add(std::make_shared<Sphere> (Vec3(0, -1000, 0), 1000, materialGround));
+  world.Add(std::make_shared<Sphere>(Vec3(0, -1000, 0), 1000, materialGround));
 
 
-  for (int i = -11; i < 11; i++) {
-    for (int j = -11; j < 11; j++){
+  constexpr int renderScatteredObjectsAmount = 11;
+  for (int i = -renderScatteredObjectsAmount; i < renderScatteredObjectsAmount; i++) {
+    for (int j = -renderScatteredObjectsAmount; j < renderScatteredObjectsAmount; j++) {
       const double chooseMaterial = RandomDouble01();
-      Vec3 center (i + 0.9 * RandomDouble01(), 0.2, j + 0.9 * RandomDouble01());
-      if((center - Vec3(4,0.2,0)).Length() > 0.9){
+      Vec3 center(i + 0.9 * RandomDouble01(), 0.2, j + 0.9 * RandomDouble01());
+      if ((center - Vec3(4, 0.2, 0)).Length() > 0.9) {
         std::shared_ptr<Material> sphere_material;
 
-        if(chooseMaterial < 0.8){
-          Color albedo  = Color::Random() * Color::Random();
+        if (chooseMaterial < 0.8) {
+          Color albedo = Color::Random() * Color::Random();
           sphere_material = std::make_shared<Lambertian>(albedo);
-          world.Add(std::make_shared<Sphere>(center,0.2,sphere_material));
+          world.Add(std::make_shared<Sphere>(center, 0.2, sphere_material));
         }
-        else if( chooseMaterial < 0.95)  {
-          Color albedo  = Color::Random(0.5,1);
-          double fuzz = RandomDouble(0.5,1);
-          sphere_material = std::make_shared<Metal>(albedo,fuzz);
-          world.Add(std::make_shared<Sphere>(center,0.2,sphere_material));
+        else if (chooseMaterial < 0.95) {
+          Color albedo = Color::Random(0.5, 1);
+          double fuzz = RandomDouble(0.5, 1);
+          sphere_material = std::make_shared<Metal>(albedo, fuzz);
+          world.Add(std::make_shared<Sphere>(center, 0.2, sphere_material));
         }
         else {
           sphere_material = std::make_shared<Dielectric>(1.5);
-          world.Add(std::make_shared<Sphere>(center,0.2,sphere_material));
+          world.Add(std::make_shared<Sphere>(center, 0.2, sphere_material));
         }
       }
     }
@@ -46,18 +47,34 @@ int main() {
   std::shared_ptr<Material> material3 = std::make_shared<Metal>(Color(0.7,0.6,0.5),0.0);
   world.Add(std::make_shared<Sphere>(Vec3(4,1,0),1.0, material3));
 
+  // Showcase Objects
+  // std::shared_ptr<Material> sampleMaterial;
+  // // sampleMaterial = std::make_shared<Lambertian>(Color::Random() * Color::Random());
+  // // sampleMaterial = std::make_shared<Dielectric>((1.5));
+  // sampleMaterial = std::make_shared<Metal>(Color(.5, .5, .5), 0.1);
+  // world.Add(std::make_shared<Sphere>(Vec3(0, 1.25, -1), 1.0, sampleMaterial));
+  // world.Add(std::make_shared<Sphere>(Vec3(0, 1.25, 1), 1.0, sampleMaterial));
+  // world.Add(std::make_shared<Sphere>(Vec3(2, 1.25, -1), 1.0, sampleMaterial));
+  // world.Add(std::make_shared<Sphere>(Vec3(2, 1.25, 1), 1.0, sampleMaterial));
+  // world.Add(std::make_shared<Sphere>(Vec3(1, 2.25, 0), 1.0, sampleMaterial));
+
 
   Camera mainCamera;
   mainCamera.m_aspectRatio = 16.0 / 9.0;
-  mainCamera.m_imgWidth = 1920;
+  mainCamera.m_imgWidth = 600;
   mainCamera.m_pixelSampleSize = 100;
-  mainCamera.m_maxDepth = 50;
+  mainCamera.m_maxDepth = 100;
 
   mainCamera.m_viewFOV = 20;
   mainCamera.m_lookFrom = Vec3(13,2,3);
   mainCamera.m_lookAt = Vec3(0,0,0);
 
-  mainCamera.m_defocusAngle = 0.6;
+  // Showcase Settings
+  // mainCamera.m_viewFOV = 30;
+  // mainCamera.m_lookFrom = Vec3(15, 5, -2.5);
+  // mainCamera.m_lookAt = Vec3(0, 1, 0.25);
+
+  mainCamera.m_defocusAngle = .6;
 
   mainCamera.Render(world);
 }
